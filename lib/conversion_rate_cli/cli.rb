e6c6ed3c195dcad644004152921e595df60fcc81
Joann Brady <jbrady11934@optimum.net>
2020-05-29 22:05:25 -0400

    cli class updates

--- lib/conversion_rate_cli/cli.rb
+++ lib/conversion_rate_cli/cli.rb
@@ -19,27 +19,30 @@ class Cli
         input = " "
         while input != "exit"
 
-            puts "Welcome to Conversion Currency CLI! 
-            puts "Conversion rates are based on 1 US Dollar."
-            puts "To get conversion rate, type 'enter'.
+            puts "Welcome to Conversion Currency CLI!" 
+            puts "Conversion Currency rates are based on 1 US Dollar."
+            puts "To get conversion rate, type 'enter'."
             puts "To quit, type 'exit'."
-    
             input = gets.strip
+            end
     
         end
 
-def enter
-  puts "Here is a list of all the Currency Codes of different countries."
-  ConversionRate.all.each.with_index(1) do |conrate.code, index|
-    puts "#{index}. #{conrate.code}" 
-  end
-  puts "Please enter number of Currency Code for exchange rate."
-  input = gets.strip.to_i
-  if input >= 1 && input <== ConversionRate.all.length
-    code = ConversionRate.all[input-1]
-    puts "#{conrate.code} = #{conrate.rate}"
-  else
-    puts "Please choose a valid number." 
-    enter
-  end
+    def enter
+        puts "Here is a list of all the Currency Codes of different countries."
+        ConversionRate.all.each.with_index(1) do |conrate.code, index|
+        puts "#{index}. #{conrate.code}" 
+        end
+        puts "Please enter number of Currency Code for exchange rate."
+        input = gets.strip.to_i
+            if input >= 1 && input <= ConversionRate.all.length
+            code = ConversionRate.all[input-1]
+            puts "#{conrate.code} = #{conrate.rate}"
+            else
+            puts "Please choose a valid number." 
+            enter
+        end
+        
+    end
+
 end 
