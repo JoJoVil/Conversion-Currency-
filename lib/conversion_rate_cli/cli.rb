7c21214c256ab745b125a754341c5fbd354e348e
Joann Brady <jbrady11934@optimum.net>
2020-05-29 21:29:04 -0400

    updating cli

--- lib/conversion_rate_cli/cli.rb
+++ lib/conversion_rate_cli/cli.rb
@@ -29,5 +29,17 @@ class Cli
         end
 
 def enter
-    
+  puts "Here is a list of all the Currency Codes of different countries."
+  ConversionRate.all.each.with_index(1) do |conrate.code, index|
+    puts "#{index}. #{conrate.code}" 
+  end
+  puts "Please enter number of Currency Code for exchange rate."
+  input = gets.strip.to_i
+  if input >= 1 && input <== ConversionRate.all.length
+    code = ConversionRate.all[input-1]
+    puts "#{conrate.code} = #{conrate.rate}"
+  else
+    puts "Please choose a valid number." 
+    enter
+  end
 end 
